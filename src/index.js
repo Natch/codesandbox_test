c3ad5e230ef75c2d765c963e34e0b4217de30179
Natch <17216462+Natch@users.noreply.github.com>
2020-10-18 22:00:05 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 /**
- * 分割代入 オブジェクト
- *
+ * スプレッド構文
  */
-// const myProfile = {
-//   name: "mario",
-//   age: 45
-// };
+const array1 = [1, 2];
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(...array1);
 
-// const message = `my name is ${myProfile.name}, age is ${myProfile.age}`;
-// console.log(message);
+const array2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...array3] = array2;
+console.log(num1);
+console.log(num2);
+console.log(array3);
 
-// const { name, age } = myProfile;
-// const message2 = `my name is ${name}, age is ${age}`;
-// console.log(message2);
+const array4 = [10, 20];
+const array5 = [30, 40];
 
-/**
- * 分割代入 配列
- */
+const array6 = [...array4];
+console.log(array6);
 
-const myProfile = ["mario", 45];
-const [name, age] = myProfile;
-const message3 = `my name is ${name}, age is ${age}`;
-console.log(message3);
+const array7 = [...array4, ...array5];
+console.log(array7);
