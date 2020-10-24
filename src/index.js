83c5b43229ac9da9a7f461d374dd63662d543f7e
Natch <17216462+Natch@users.noreply.github.com>
2020-10-24 17:57:02 +0900

    未完了TODOからの削除処理

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import "./styles.css";
 
 const onClickAdd = () => {
+  // 入力の受け取り
   const inputElement = document.getElementById("add-text");
   const inputText = inputElement.value;
   inputElement.value = "";
 
+  // 未完了TODO：テキスト
   const div = document.createElement("div");
   div.className = "list-row";
 
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // 未完了TODO：完了ボタン
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("complete");
+  });
+
+  // 未完了TODO：削除ボタン
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
   document.getElementById("incomplete-list").appendChild(div);
 };
 
