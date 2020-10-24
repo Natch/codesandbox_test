a0a5151fe94c92b5ee2acbb0df04b89694a954e2
Natch <17216462+Natch@users.noreply.github.com>
2020-10-24 17:41:15 +0900

    未完了TODOの追加処理

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputElement = document.getElementById("add-text");
+  const inputText = inputElement.value;
+  inputElement.value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  div.appendChild(li);
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
